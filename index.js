a99d2765d16ebddfa6aad95d54baa4b23e02ca86
kashelton17 <kashelton17@gmail.com>
2021-06-27 21:07:10 -0400

    adding questions to app

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const inq = require('inquirer')
 const query = require('./lib/js/query.js')
 
+const questions = ['View all employees', 'View all employees by department', 'View all employees by role', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Udate employee manager', 'Remove employee', 'Remove department', 'Remove role']
+
 const getQuestions = () => {
-    
+    inq 
+        .prompt([
+            {
+                type: 'list',
+                name: 'answer',
+                message: 'What do you want to do?',
+                choices: questions
+            },
+        ])
+        .then(answer => {
+            console.log(answer.answer)
+        })
 }
