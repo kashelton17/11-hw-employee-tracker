912c335f0e246920e157fe4f66a203f095310a64
kashelton17 <kashelton17@gmail.com>
2021-06-28 22:22:24 -0400

    able to add roles and departments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inq = require('inquirer')
+require('console.table')
 const mysql = require('mysql');
-const table = require('console.table')
 require('dotenv').config()
 
 const questions = ['View all employees', 'View departments', 'View roles', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Udate employee manager', 'Remove employee', 'Remove department', 'Remove role', 'Exit']
@@ -17,16 +17,20 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
+
 connection.connect(function (err) {
-    const query1 = 'SELECT roles.id "ID", roles.title "Title", roles.salary "Salary" FROM roles' 
+    if (err) throw err
+
+    const query1 = 'SELECT roles.id "ID", roles.title "Title", roles.salary "Salary" FROM roles;' 
     connection.query(query1, (err, res)=> {
         if (err) throw err
-        roles =  res.map(role => ({name: role.Title, value: role.ID}))
+        roles = res.map(role => ({name: role.Title, value: role.ID}))
     })
     
-    const query2 = 'SELECT * from department'
-    connection.query(query2, function (error, res) {
-        depts = res.map(dep => ({ name: dep.name, value: dep.id }))
+    const query2 = 'SELECT * from departments;'
+    connection.query(query2, function (err, res) {
+        if (err) throw err
+        depts = res.map(dep => ({name: dep.name, value: dep.id}))
       })
     
     const query3 =  'SELECT CONCAT(first_name, " ", last_name) "Name", employees.id "ID" FROM employees LEFT JOIN roles ON role_id = roles.id WHERE roles.title = "Manager";'
@@ -35,10 +39,10 @@ connection.connect(function (err) {
         managers =  res.map(manager => ({name: manager.Name, value: manager.ID}))
     })
 
-    const query4 = 'SELECT CONCAT(first_name, " ", last_name) "Name", id "ID" FROM employees'
+    const query4 = 'SELECT * FROM employees;'
     connection.query(query4, (err, res) => {
         if (err) throw err
-        employees = res.map(emp => ({name: emp.Name, value: emp.ID}))
+        employees = res.map(emp => ({name: `${emp.first_name} ${emp.last_name}`, value: emp.ID}))
     })
 
     getQuestions()
@@ -194,7 +198,7 @@ const getQuestions = () => {
                         {
                             type: 'input',
                             name: 'roleTitle',
-                            message: 'What is the name of the role?'
+                            message: 'What is the name of the new role?'
                         },
                         {
                             type: 'input',
@@ -229,7 +233,6 @@ const getQuestions = () => {
                     ])
                     .then(answers => {
                         updateRole([{role_id: answers.newRole}, {id: answers.updateE}])
-
                     })
             } else if (answer.choice === 'Update employee manager'){
                 query9
