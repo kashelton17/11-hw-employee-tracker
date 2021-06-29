1774370ab041545991d6bee8bd9809131931cbb0
kashelton17 <kashelton17@gmail.com>
2021-06-28 20:57:16 -0400

    adding queries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inq = require('inquirer')
 const query = require('./lib/js/query.js')
 
-const questions = ['View all employees', 'View all employees by department', 'View all employees by role', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Udate employee manager', 'Remove employee', 'Remove department', 'Remove role', 'Exit']
+const questions = ['View all employees', 'View departments', 'View roles', 'Add employee', 'Add department', 'Add role', 'Update employee role', 'Udate employee manager', 'Remove employee', 'Remove department', 'Remove role', 'Exit']
 
 const getQuestions = () => {
     inq 
@@ -17,21 +17,83 @@ const getQuestions = () => {
             console.log(answer.choice)
             if (answer.choice === 'View all employees'){
                 query.viewEmployees()
-                getQuestions()
-            } else if (answer.choice === 'View all employees by department'){
-                query2
-            } else if (answer.choice === 'View all employees by role'){
-                query3
+                startOver()
+            } else if (answer.choice === 'View departments'){
+                query.viewDepartments()
+                    (startOver())
+            } else if (answer.choice === 'View roles'){
+                query.viewRoles()
+                    (startOver())
             } else if (answer.choice === 'Add employee'){
-                query4
+
+                inq
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'firstName',
+                            message: 'Input employee first name'
+                        },
+                        {
+                            type: 'input',
+                            name: 'lastName',
+                            message: 'Input employee last name'
+                        },
+                        {
+                            type: 'list',
+                            name: 'chosenRole',
+                            message: 'Choose a role',
+                            choices: query.showroles
+                        },
+                        {
+                            type: 'list',
+                            name: 'chosenManager',
+                            message: 'Choose a Manager',
+                            choices: query.showemployees
+                        }
+                    ])
+                    .then(answers => {
+                        const object = {first_name: answers.firstName, last_name: answers.lastName, role_id: answers.chosenRole.ID, manager_id: answers.chosenManager.ID}
+                        query.addEmployee(object)
+                        startOver()
+                    })
             } else if (answer.choice === 'Add department'){
-                query5
+                inq
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'dept',
+                            message: 'What is the name of the department?'
+                        }
+                    ])
+                    .then(answer => {
+                        query.addDepartment({ name: answer.dept})
+                        startOver()
+                    })
             } else if (answer.choice === 'Add role'){
-                query6
+                inq 
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'roleTitle',
+                            message: 'What is the name of the role?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'roleSalary',
+                            message: 'What is the salary of the role?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'roleDept',
+                            message: 'What is the department id?'
+                        }
+                    ])
+                    .then(answers => {
+                        query.addRole({title: answers.roleTitle, salary: answers.roleSalary, department_id: answers.roleDept})
+                        startOver()
+                    })
             } else if (answer.choice === 'Update employee role'){
                 query7
-            } else if (answer.choice === 'Update employee role'){
-                query8
             } else if (answer.choice === 'Update employee manager'){
                 query9
             } else if (answer.choice === 'Remove employee'){
@@ -43,6 +105,26 @@ const getQuestions = () => {
             } else if (answer.choice === 'Remove role') {
                 query12
             } else if (answer.choice === 'Exit'){
+                return 'Goodbye'
+            }
+        })
+}
+
+const startOver = () => {
+    inq
+        .prompt([
+            {
+                type: 'list',
+                name: 'confirm',
+                message: 'Do you want to continue or exit?',
+                choices: ['Continue', 'Exit']
+            }
+        ])
+        .then(answer => {
+            if (answer === 'Continue') {
+                getQuestions()
+            } else {
+                query.connection.end
                 return 'Goodbye'
             }
         })
